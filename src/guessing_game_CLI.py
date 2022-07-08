6308508cdeb43ee6278a7a460b422a4ae8316114
Shlomo isaacs <shloimyisaacs@gmail.com>
2022-07-08 13:15:39 -0700

    Fix a lot of mistakes
    fixed a ton of spelling mistakes

--- src/guessing_game_CLI.py
+++ src/guessing_game_CLI.py
@@ -3,24 +3,25 @@ import random
 import os 
 import time 
 
-# Printing out cretits.
+# Printing out credits.
 print('''
 ############ Credits ############
 #           Panguin6010         #
 #           Nosson-P            #
+#           Llamalectric        #
 #           StackOverflow       #
 ##################################
 ''')
 
 print("Please wait 2 seconds.")
 time.sleep(1)
-print("CTR + C to Exit")
+print("CTRL + C to Exit")
 time.sleep(1)
 os.system("cls" if os.name == "nt" else "clear")
 # Amount of guesses taken.
 guessesTaken = 1
 users_name = os.getlogin()
-# Asking user what the top number should be, as well as how many guess they should have.
+# Asking user what the top number should be, as well as how many guesses they should have.
 # Making sure user only inputs str and int in the right places.
 while True:
     try:
@@ -39,42 +40,41 @@ while True:
 
         print("Ok lets start!")
 
-                # Generating sudo random number.
+                # Generating pseudorandom number.
         answer = random.randint( 1 , top_value_of_random_number )
 
         for guessesTaken in range(number_of_guesses):
 
             guess = int(input("Take a guess!:\n"))
-            # Guess is to low.
+            # Guess is too low.
             if guess < answer:
                 print('your guess is too low ↓:\n')
     
-            # Guess is to high.
+            # Guess is too high.
             if guess > answer: 
                 print('your guess is too high ↑:\n')
     
-            # If users guesses correctlty exit loop.
+            # If users guesses correctly exit loop.
             if guess == answer:
 
                 break
 
     except ValueError:
 
-        # Input's are wrong back to start of loop.
+        # If the inputs are wrong return to start of loop.
         os.system("cls" if os.name == "nt" else "clear")
         print("Sorry, I don't understand you might have typed it in wrong lets try again from the begining.")
 
         continue
 
     else:
-        # Input's are good exiting loop.
+        # If the inputs are good exit the loop.
         break
 
-# If users guesses tell users they got it right along with some info. 
+# If user guesses tell user they got it right along with some info. 
 if guess == answer:
-#    GuessesTaken = str(guessesTaken + 1)8.
-    print("great job, " + users_name + "! You guessed the number in " + repr(guessesTaken) + " turns! out of " + repr(number_of_guesses) +  " turns the number was " + repr(answer) + ":")
-    print("if you want to stop play press CTR + C.")
-# If not guessed in 6 trys convert answer to string and tell users that they did not get it and show the number.
+    print("Great job, " + users_name + "! You guessed the number in " + repr(guessesTaken) + " turns! The number of turns was  " + repr(number_of_guesses) +  " turns. The number was " + repr(answer) + ":")
+    print("if you want to stop play press CTRL + C.")
+# If the number is not guessed in 6 trys convert answer to string and tell users that they did not get it and show the number.
 if guess != answer:    
-    print("Sorry, " + users_name + " you did not guess the number I was thinking of in " + repr(number_of_guesses) + " trys The number I was thinking of was " +  repr(answer) + " try agian.")
+    print("Sorry, " + users_name + " you did not guess the number I was thinking of in " + repr(number_of_guesses) + " trys. The number I was thinking of was " +  repr(answer) + " try again.")
